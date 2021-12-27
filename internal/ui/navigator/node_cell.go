ef8e0d95044552d3e64fbd338f8257a8f8d38f12
Richard Wilkes <wilkes@me.com>
2021-12-27 09:22:27 -0800

    Make use of the new Drawable support

--- internal/ui/navigator/node_cell.go
+++ internal/ui/navigator/node_cell.go
@@ -1,49 +1,21 @@
 package navigator
 
 import (
-	"github.com/richardwilkes/gcs/internal/ui/widget"
 	"github.com/richardwilkes/toolbox/xmath/geom32"
 	"github.com/richardwilkes/unison"
 )
 
 func createNodeCell(ext, title string) *unison.Panel {
-	panel := unison.NewPanel()
-	panelLayout := &unison.FlexLayout{
-		Columns:  2,
-		HSpacing: 4,
-	}
-	panel.SetLayout(panelLayout)
-	s := unison.LabelFont.ResolvedFont().Size() + 5
-	size := geom32.NewSize(s, s)
+	size := unison.LabelFont.ResolvedFont().Size() + 5
 	info, ok := fileTypes[ext]
 	if !ok {
 		info, ok = fileTypes["file"]
 	}
-	if ok {
-		p := info.svg.PathForSize(size)
-		svgSize := info.svg.Size()
-		if svgSize.Width != svgSize.Height {
-			p = p.NewTranslatedPt(info.svg.OffsetToCenterWithinScaledSize(size))
-		}
-		icon := widget.NewIcon()
-		icon.Path = p
-		icon.Size = size
-		icon.SetLayoutData(&unison.FlexLayoutData{
-			HSpan: 1,
-			VSpan: 1,
-		})
-		panel.AddChild(icon)
-	} else {
-		panelLayout.Columns = 1
-	}
 	label := unison.NewLabel()
 	label.Text = title
-	label.SetLayoutData(&unison.FlexLayoutData{
-		HSpan: 1,
-		VSpan: 1,
-		HGrab: true,
-	})
-	panel.AddChild(label)
-	panel.NeedsLayout = true
-	return panel
+	label.Drawable = &unison.DrawableSVG{
+		SVG:  info.svg,
+		Size: geom32.NewSize(size, size),
+	}
+	return label.AsPanel()
 }
