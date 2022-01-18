32688d5071d153c021330cda8aa6478b0d23a5b0
Richard Wilkes <wilkes@me.com>
2022-01-18 13:21:46 -0800

    Use JSON load/save support from toolbox

--- internal/gurps/body_type.go
+++ internal/gurps/body_type.go
@@ -13,33 +13,23 @@ package gurps
 
 import (
 	"encoding/json"
-	"io"
-	"io/fs"
 	"os"
 	"path/filepath"
 
 	"github.com/richardwilkes/rpgtools/dice"
 	"github.com/richardwilkes/toolbox/errs"
 	"github.com/richardwilkes/toolbox/log/jot"
-	"github.com/richardwilkes/toolbox/xio"
-	"github.com/richardwilkes/toolbox/xio/fs/safe"
-)
-
-// The data format versions for BodyType.
-const (
-	BodyTypeCurrentVersion = 3
-	BodyTypeJavaVersion    = 2
+	xfs "github.com/richardwilkes/toolbox/xio/fs"
 )
 
 // BodyType holds a set of hit locations.
 type BodyType struct {
-	BodyTypeStorage
-	owningLocation *HitLocation
+	BodyTypeStorage `json:",inline"`
+	owningLocation  *HitLocation
 }
 
 // BodyTypeStorage defines the current BodyType data format.
 type BodyTypeStorage struct {
-	Version   int            `json:"version"`
 	ID        string         `json:"id"`
 	Name      string         `json:"name"`
 	Roll      dice.Dice      `json:"roll"`
@@ -48,42 +38,22 @@ type BodyTypeStorage struct {
 
 // FactoryBodyType returns a new copy of the factory BodyType.
 func FactoryBodyType() *BodyType {
-	b, err := LoadBodyType(embeddedFS, "data/body_types/Humanoid.body")
-	jot.FatalIfErr(err)
-	return b
-}
-
-// LoadBodyType creates a BodyType from a file.
-func LoadBodyType(fileSystem fs.FS, filePath string) (*BodyType, error) {
-	f, err := fileSystem.Open(filePath)
-	if err != nil {
-		return nil, errs.NewWithCause("unable to open body type file", err)
-	}
-	defer xio.CloseIgnoringErrors(f)
-	var bt BodyType
-	if err = json.NewDecoder(f).Decode(&bt); err != nil {
-		return nil, errs.NewWithCause("invalid body type file: "+filePath, err)
-	}
-	return &bt, nil
-}
-
-// MarshalJSON implements json.Marshaler. Sets the current version on the output.
-func (b *BodyType) MarshalJSON() ([]byte, error) {
-	b.Version = BodyTypeCurrentVersion
-	return json.Marshal(&b.BodyTypeStorage)
+	var bodyType BodyType
+	jot.FatalIfErr(xfs.LoadJSONFromFS(embeddedFS, "data/body_types/Humanoid.body", &bodyType))
+	return &bodyType
 }
 
 // UnmarshalJSON implements json.Unmarshaler. Loads the current format as well as older variants.
 func (b *BodyType) UnmarshalJSON(data []byte) error {
 	var variants struct {
-		BodyTypeStorage `json:",inline"` // v3+, except for the Version field, which is v0+
-		Type            string           `json:"type"`          // v0-2
-		HitLocations    BodyTypeStorage  `json:"hit_locations"` // v0-2
+		BodyTypeStorage `json:",inline"`
+		Type            string          `json:"type"`
+		HitLocations    BodyTypeStorage `json:"hit_locations"`
 	}
 	if err := json.Unmarshal(data, &variants); err != nil {
 		return err
 	}
-	if variants.Version <= BodyTypeJavaVersion && variants.Type == "hit_locations" {
+	if variants.Type == "hit_locations" {
 		b.BodyTypeStorage = variants.HitLocations
 	} else {
 		b.BodyTypeStorage = variants.BodyTypeStorage
@@ -96,15 +66,8 @@ func (b *BodyType) SaveTo(filePath string, entity *Entity) error {
 	if err := os.MkdirAll(filepath.Dir(filePath), 0o750); err != nil {
 		return errs.NewWithCause(filePath, err)
 	}
-	if err := safe.WriteFileWithMode(filePath, func(w io.Writer) error {
-		b.calc(entity)
-		encoder := json.NewEncoder(w)
-		encoder.SetIndent("", "  ")
-		return encoder.Encode(b)
-	}, 0o640); err != nil {
-		return errs.NewWithCause(filePath, err)
-	}
-	return nil
+	b.calc(entity)
+	return xfs.SaveJSONWithMode(filePath, b, true, 0o640)
 }
 
 func (b *BodyType) calc(entity *Entity) {
