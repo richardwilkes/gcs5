ac0008172da97e6463658b5ec54b3ad2fef26d0b
Richard Wilkes <wilkes@me.com>
2022-01-27 18:14:10 -0800

    Partial impl of Weapon

--- model/gurps/weapon.go
+++ model/gurps/weapon.go
@@ -11,26 +11,40 @@
 
 package gurps
 
+import (
+	"github.com/richardwilkes/gcs/model/encoding"
+	"github.com/richardwilkes/toolbox/xio"
+	"github.com/richardwilkes/toolbox/xmath"
+	"github.com/richardwilkes/toolbox/xmath/fixed"
+)
+
 const (
-	weaponTypeKey       = "type"
-	weaponReachKey      = "reach"
-	weaponParryKey      = "parry"
-	weaponBlockKey      = "block"
-	weaponAccuracyKey   = "accuracy"
-	weaponRangeKey      = "range"
-	weaponRateOfFireKey = "rate_of_fire"
-	weaponShotsKey      = "shots"
-	weaponBulkKey       = "bulk"
-	weaponRecoilKey     = "recoil"
-	weaponDefaultsKey   = "defaults"
-	weaponStrengthKey   = "strength"
-	weaponUsageKey      = "usage"
-	weaponUsageNotesKey = "usage_notes"
+	weaponTypeKey            = "type"
+	weaponDamageKey          = "damage"
+	weaponMinimumStrengthKey = "strength"
+	weaponReachKey           = "reach"
+	weaponParryKey           = "parry"
+	weaponBlockKey           = "block"
+	weaponAccuracyKey        = "accuracy"
+	weaponRangeKey           = "range"
+	weaponRateOfFireKey      = "rate_of_fire"
+	weaponShotsKey           = "shots"
+	weaponBulkKey            = "bulk"
+	weaponRecoilKey          = "recoil"
+	weaponDefaultsKey        = "defaults"
+	weaponUsageKey           = "usage"
+	weaponUsageNotesKey      = "usage_notes"
+	weaponCalcLevelKey       = "level"
+	weaponCalcParryKey       = "parry"
+	weaponCalcBlockKey       = "block"
+	weaponCalcDamageKey      = "damage"
+	weaponCalcRangeKey       = "range"
 )
 
 // Weapon holds the stats for a weapon.
 type Weapon struct {
 	Type            WeaponType
+	Damage          *WeaponDamage
 	MinimumStrength string
 	Usage           string
 	UsageNotes      string
@@ -43,6 +57,122 @@ type Weapon struct {
 	Shots           string
 	Bulk            string
 	Recoil          string
-	Damage          *WeaponDamage
 	Defaults        []*SkillDefault
+}
+
+// NewWeaponFromJSON creates a new Weapon from a JSON object.
+func NewWeaponFromJSON(data map[string]interface{}) *Weapon {
+	w := &Weapon{
+		Type:            WeaponTypeFromKey(encoding.String(data[weaponTypeKey])),
+		MinimumStrength: encoding.String(data[weaponMinimumStrengthKey]),
+		Usage:           encoding.String(data[weaponUsageKey]),
+		UsageNotes:      encoding.String(data[weaponUsageNotesKey]),
+	}
+	w.Damage = NewWeaponDamageFromJSON(w, encoding.Object(data[weaponDamageKey]))
+	switch w.Type {
+	case Melee:
+		w.Reach = encoding.String(data[weaponReachKey])
+		w.Parry = encoding.String(data[weaponParryKey])
+		w.Block = encoding.String(data[weaponBlockKey])
+	case Ranged:
+		w.Accuracy = encoding.String(data[weaponAccuracyKey])
+		w.Range = encoding.String(data[weaponRangeKey])
+		w.RateOfFire = encoding.String(data[weaponRateOfFireKey])
+		w.Shots = encoding.String(data[weaponShotsKey])
+		w.Bulk = encoding.String(data[weaponBulkKey])
+		w.Recoil = encoding.String(data[weaponRecoilKey])
+	}
+	array := encoding.Array(data[weaponDefaultsKey])
+	if len(array) != 0 {
+		w.Defaults = make([]*SkillDefault, len(array))
+		for i, one := range array {
+			w.Defaults[i] = NewSkillDefaultFromJSON(false, encoding.Object(one))
+		}
+	}
+	return w
+}
+
+// ToJSON emits this object as JSON.
+func (w *Weapon) ToJSON(encoder *encoding.JSONEncoder) {
+	encoder.StartObject()
+	encoder.KeyedString(weaponTypeKey, w.Type.Key(), false, false)
+	encoding.ToKeyedJSON(w.Damage, weaponDamageKey, encoder)
+	encoder.KeyedString(weaponMinimumStrengthKey, w.MinimumStrength, true, true)
+	encoder.KeyedString(weaponUsageKey, w.Usage, true, true)
+	encoder.KeyedString(weaponUsageNotesKey, w.UsageNotes, true, true)
+	switch w.Type {
+	case Melee:
+		encoder.KeyedString(weaponReachKey, w.Reach, true, true)
+		encoder.KeyedString(weaponParryKey, w.Parry, true, true)
+		encoder.KeyedString(weaponBlockKey, w.Block, true, true)
+	case Ranged:
+		encoder.KeyedString(weaponAccuracyKey, w.Accuracy, true, true)
+		encoder.KeyedString(weaponRangeKey, w.Range, true, true)
+		encoder.KeyedString(weaponRateOfFireKey, w.RateOfFire, true, true)
+		encoder.KeyedString(weaponShotsKey, w.Shots, true, true)
+		encoder.KeyedString(weaponBulkKey, w.Bulk, true, true)
+		encoder.KeyedString(weaponRecoilKey, w.Recoil, true, true)
+	}
+	if len(w.Defaults) != 0 {
+		encoder.Key(weaponDefaultsKey)
+		encoder.StartArray()
+		for _, one := range w.Defaults {
+			one.ToJSON(false, encoder)
+		}
+		encoder.EndArray()
+	}
+	// Emit the calculated values for third parties
+	encoder.Key(calcKey)
+	encoder.StartObject()
+	encoder.KeyedNumber(weaponCalcLevelKey, fixed.F64d4FromInt64(int64(xmath.MaxInt(w.SkillLevel(), 0))), true)
+	switch w.Type {
+	case Melee:
+		encoder.KeyedString(weaponCalcParryKey, w.ResolvedParry(nil), true, true)
+		encoder.KeyedString(weaponCalcBlockKey, w.ResolvedBlock(nil), true, true)
+	case Ranged:
+		encoder.KeyedString(weaponCalcRangeKey, w.ResolvedRange(), true, true)
+	}
+	encoder.KeyedString(weaponCalcDamageKey, w.Damage.ResolvedDamage(nil), true, true)
+	encoder.EndObject()
+	encoder.EndObject()
+}
+
+// SkillLevel returns the resolved skill level.
+func (w *Weapon) SkillLevel() int {
+	// TODO: Implement
+	return 0
+}
+
+// ResolvedParry returns the resolved parry level.
+func (w *Weapon) ResolvedParry(tooltip *xio.ByteBuffer) string {
+	// TODO: Implement
+	return ""
+}
+
+// ResolvedBlock returns the resolved block level.
+func (w *Weapon) ResolvedBlock(tooltip *xio.ByteBuffer) string {
+	// TODO: Implement
+	return ""
+}
+
+// ResolvedRange returns the range, fully resolved for the user's ST, if possible.
+func (w *Weapon) ResolvedRange() string {
+	// TODO: Implement
+	return ""
+}
+
+// ResolvedMinimumStrength returns the resolved minimum strength required to use this weapon, or 0 if there is none.
+func (w *Weapon) ResolvedMinimumStrength() int {
+	started := false
+	value := 0
+	for _, ch := range w.MinimumStrength {
+		if ch >= '0' && ch <= '9' {
+			value *= 10
+			value += int(ch - '0')
+			started = true
+		} else if started {
+			break
+		}
+	}
+	return value
 }
