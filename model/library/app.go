49d0a6df57be9730b0377912f06cfebf17fa59d6
Richard Wilkes <wilkes@me.com>
2022-06-16 11:30:02 -0700

    In-progress changes for app update check

--- model/library/app.go
+++ model/library/app.go
@@ -26,9 +26,9 @@ import (
 )
 
 type appUpdater struct {
-	lock    sync.RWMutex
-	result  string
-	release *Release
+	lock     sync.RWMutex
+	result   string
+	releases []Release
 }
 
 var appUpdate appUpdater
@@ -36,19 +36,14 @@ var appUpdate appUpdater
 func (u *appUpdater) Reset() {
 	u.lock.Lock()
 	u.result = fmt.Sprintf(i18n.Text("Checking for %s updates…"), cmdline.AppName)
-	u.release = nil
+	u.releases = nil
 	u.lock.Unlock()
 }
 
-func (u *appUpdater) Result() (string, *Release) {
+func (u *appUpdater) Result() (string, []Release) {
 	u.lock.RLock()
 	defer u.lock.RUnlock()
-	var release *Release
-	if u.release != nil {
-		other := *u.release
-		release = &other
-	}
-	return u.result, release
+	return u.result, u.releases
 }
 
 func (u *appUpdater) SetResult(str string) {
@@ -57,16 +52,15 @@ func (u *appUpdater) SetResult(str string) {
 	u.lock.Unlock()
 }
 
-func (u *appUpdater) SetRelease(release *Release) {
+func (u *appUpdater) SetReleases(releases []Release) {
 	u.lock.Lock()
-	u.result = fmt.Sprintf(i18n.Text("%s v%s is available!"), cmdline.AppName, release.Version)
-	other := *release
-	u.release = &other
+	u.result = fmt.Sprintf(i18n.Text("%s v%s is available!"), cmdline.AppName, releases[0].Version)
+	u.releases = releases
 	u.lock.Unlock()
 }
 
 func (u *appUpdater) NotifyOfUpdate() {
-	if title, release := u.Result(); release != nil {
+	if title, releases := u.Result(); releases != nil {
 		// TODO: Show release notes of all releases between the version running and the version available.
 		unison.QuestionDialog(title, "")
 	}
@@ -84,7 +78,7 @@ func CheckForAppUpdates() {
 		defer cancel()
 		releases, err := LoadReleases(ctx, &http.Client{}, "richardwilkes", "gcs", cmdline.AppVersion,
 			func(version, notes string) bool {
-				return txt.NaturalLess(version, "5.0.0", true)
+				return txt.NaturalLess(version, "5.0.0", true) // Don't bother showing changes from before 5.0, since those were the Java version
 			})
 		if err != nil {
 			appUpdate.SetResult(fmt.Sprintf(i18n.Text("Unable to access the %s update site"), cmdline.AppName))
@@ -92,22 +86,22 @@ func CheckForAppUpdates() {
 			return
 		}
 		if len(releases) == 0 || releases[0].Version == cmdline.AppVersion {
-			appUpdate.SetResult(fmt.Sprintf(i18n.Text("%s has no update available"), cmdline.AppName))
+			appUpdate.SetResult(fmt.Sprintf(i18n.Text("No %s updates are available"), cmdline.AppName))
 			return
 		}
-		appUpdate.SetRelease(&releases[0])
+		appUpdate.SetReleases(releases)
 		unison.InvokeTask(appUpdate.NotifyOfUpdate)
 	}()
 }
 
 // AppUpdateResult returns the current results of any outstanding app update check.
-func AppUpdateResult() (string, *Release) {
+func AppUpdateResult() (string, []Release) {
 	return appUpdate.Result()
 }
 
 // AppUpdate will perform the application update or take the user to the website so they can do it themselves.
 func AppUpdate() {
-	if _, release := appUpdate.Result(); release != nil {
+	if _, releases := appUpdate.Result(); releases != nil {
 		// TODO: Do the update
 	}
 }
